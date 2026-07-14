ffe2291be766bf8c130b70171ce304a19d1a4748
marianneoliveirxr <marianne.rodrigues@sptech.school>
2026-07-14 13:14:08 -0300

    Add: instruções SQL criar perfil

--- src/models/perfilModel.js
+++ src/models/perfilModel.js
@@ -1,3 +1,4 @@
+const { add } = require("nodemon/lib/rules");
 var database = require("../database/config");
 
 function adicionarPerfil(perfilId, id_usuario, peso, altura, pesoMeta){
@@ -9,6 +10,24 @@ function adicionarPerfil(perfilId, id_usuario, peso, altura, pesoMeta){
             return database.executar(instrucaoSql).then(() => addProgresso(peso, altura, id_usuario, pesoMeta));
 }
 
+function addProgresso(peso, altura, id_usuario, pesoMeta){
+    var instrucaoSql = `
+        INSERT INTO progresso (peso, altura, fkUsuario) VALUES (${peso}, ${altura}, ${id_usuario});
+    `;
+    console.log("Executanto a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql).then(() => adicionarMeta(pesoMeta, id_usuario));
+}
+
+function adicionarMeta(pesoMeta, id_usuario){
+    var instrucaoSql = `
+    INSERT INTO meta (pesoMeta, fkUsuario) VALUES (${pesoMeta}, ${id_usuario});
+    `;
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
 module.exports = {
-    adicionarPerfil
+    adicionarPerfil,
+    addProgresso,
+    adicionarMeta
 }
